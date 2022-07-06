455d25094c5c4a362f251e691107a44ae9b82b5a
Calogero Sortino <calogerosortino209@gmail.com>
2022-07-06 20:50:31 +0200

    adding testing file

--- ols_linear_reg.py
+++ ols_linear_reg.py
@@ -114,10 +114,3 @@ class ols_lr():
 
 
 
-df = pd.read_csv('mroz.csv')
-
-print(df)
-
-obj = ols_lr(data = df,y = 'lwage', x = ['exper', 'expersq', 'educ', 'age', 'kidslt6', 'kidsge6'], method ='robust')
-
-print(obj.summary())
---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import ols_linear_reg as ols
+
+df = pd.read_csv('mroz.csv')
+
+print(df)
+
+obj = ols.ols_lr(data = df,y = 'lwage', x = ['exper', 'expersq', 'educ', 'age', 'kidslt6', 'kidsge6'], method ='robust')
+
+print(obj.summary())
