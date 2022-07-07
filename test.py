b7155831ce2f1ee9c1c719e027481267823bf9ff
Calogero Sortino <calogerosortino209@gmail.com>
2022-07-07 11:37:19 +0200

    added wald test in robust way

--- test.py
+++ test.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 import ols_linear_reg as ols
 
-df = pd.read_csv('mroz.csv')
+df = pd.read_csv('body.csv')
 
 print(df)
 
-obj = ols.ols_lr(data = df,y = 'lwage', x = ['exper', 'expersq', 'educ', 'age', 'kidslt6', 'kidsge6'], method ='robust')
+obj = ols.ols_lr(data = df,y = 'lbwght', x = ['male', 'parity', 'lfaminc', 'packs'])
+
+diagnostic = ols.diagnostic(obj, ['packs'])
 
 print(obj.summary())
 
+print(diagnostic.F_test())
 
 
-diagno = ols.diagnostic(obj,var_to_test=['age', 'kidslt6', 'kidsge6'])
-
-print(diagno.wald_test())
