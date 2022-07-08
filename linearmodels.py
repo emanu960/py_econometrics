196c678a6dcfcc72ab4a4f9965df5f9de84d9ed4
Calogero Sortino <calogerosortino209@gmail.com>
2022-07-08 15:26:51 +0200

    adding 2sls and compacting ols and 2sls in linearmodules. ols linear reg is temporary module. I will delete it once i put tests in the right place

--- linearmodels.py
+++ linearmodels.py
@@ -123,140 +123,124 @@ class ols_lr():
         vec = list(map(list, zip(*vec)))
 
 
-
-
+        print('Linear regression')
         print('---------------------------------------------------------------')
         print(tabulate(vec, headers=header))
         print('---------------------------------------------------------------')
         return ' '
 
 
+class two_sls():
+    def __init__(self, data , exogenous,endogenous, instruments, y, method = 'non_robust'):
+        self.exogenous = exogenous
+        self.endogenous = endogenous
+        self.instruments = instruments
+        self.y = y
+        self.data = data
 
-################################################################à
+        self.method = method
 
+    def prepare_df_first_stage(self):
+        df = self.data
+        ones = np.ones(len(df))
+        df['cons'] = ones
+        first_stage_keys =  self.endogenous + self.exogenous +self.instruments + ['cons']
+        df_first_stage = df[first_stage_keys].dropna()
+        Z = df_first_stage.drop(self.endogenous,axis = 1).to_numpy()
+        xk_vec = []
+        for k in self.endogenous:
+            xk_vec.append(df_first_stage[k].to_numpy())
 
+        return {'xk': np.transpose(np.array(xk_vec)), 'Z': Z, 'df': df_first_stage}
 
-# 2 STAGE LEAST SQUARES ESTIMATOR
+    def compute_x_hat(self):
+        Z = self.prepare_df_first_stage().get('Z')
+        Xk = self.prepare_df_first_stage().get('xk')
+        df = self.prepare_df_first_stage().get('df')
+        z_features = self.exogenous + self.instruments
+        X_hat = []
+        for i in range(Xk.shape[1]):
+            reg = ols_lr(data = df, y = self.endogenous[i], x = z_features)
+            fitted = reg.fitted()
+            X_hat.append(fitted)
 
-
-################################################################à
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return np.transpose(np.array(X_hat))[0]
 
 
+    def prepare_df_second_stage(self):
+        X_hat = self.compute_x_hat()
+        df_full = self.data
+        for i in range(len(self.endogenous)):
+            df_full[f'fitted_{self.endogenous[i]}'] = X_hat[:,i]
+
+        fitted_keys = [i for i in df_full.columns.to_list() if i.split('_')[0] == 'fitted']
+        df_full['cons'] = np.ones(len(df_full))
+
+        full_keys = ['cons'] + self.exogenous + fitted_keys + [self.y] + self.endogenous
+        df_for_reg = df_full[full_keys].dropna()
+
+        xhat_keys =['cons'] + self.exogenous +fitted_keys
+        x_keys =   ['cons'] + self.exogenous+ self.endogenous
+
+        X = df_for_reg[x_keys].to_numpy()
+        Y = df_for_reg[self.y].to_numpy().reshape(len(df_for_reg), 1)
+        X_hat_final=  df_for_reg[xhat_keys].to_numpy().reshape(len(df_for_reg),len(xhat_keys))
+
+        return {'X': X, 'Y': Y, 'X_hat': X_hat_final}
+
+    def betas(self):
+            X_hat_final = self.prepare_df_second_stage().get('X_hat')
+            X =self.prepare_df_second_stage().get('X')
+            Y = self.prepare_df_second_stage().get('Y')
+
+            inve = np.linalg.inv(np.matmul(np.transpose(X_hat_final), X))
+            final_part = np.matmul(np.transpose(X_hat_final), Y)
+            beta_iv = np.matmul(inve, final_part)
+            return beta_iv
+
+    def std(self):
+        X = self.prepare_df_second_stage().get('X')
+        Y = self.prepare_df_second_stage().get('Y')
+        X_hat = self.prepare_df_second_stage().get('X_hat')
+        beta = (self.betas())
+
+        residuals = Y - np.matmul(X, beta)
+        ssr = np.matmul(np.transpose(residuals), residuals)
+        sigma_hat = ssr/ (len(X)- X.shape[1])
+        SIGMA = sigma_hat * np.linalg.inv(np.matmul(np.transpose(X_hat), X_hat))
+        std_dv = []
+        for i in range(SIGMA.shape[0]):
+            for j in range(SIGMA.shape[1]):
+                if i ==j:
+                    std_dv.append(np.sqrt(SIGMA[i,j]))
+
+        return std_dv
+
+
+    def summary(self):
+        header = [self.y, 'coefficient', 'se']
+        table = []
+        vars =  ['cons' ] + self.exogenous+ self.endogenous
+        vec = [vars, self.betas(), self.std()]
+        vec = list(map(list, zip(*vec)))
+
+        print('2SlS Regression')
+        print('---------------------------------------------------------------')
+        print(tabulate(vec, headers=header))
+        print('---------------------------------------------------------------')
+        print(f'INSTRUMENTED: {self.endogenous}')
+        print(f'INSTRUMENTS: {self.exogenous+self.instruments}')
+
+        return ' '
 
 
 
-#---------------------------------------------------------------------
+df = pd.read_csv('mroz.csv')
 
-
-#                        DIAGNOSTIC                            #
-
-
-#---------------------------------------------------------------------
+obj = two_sls(data = df, exogenous=['exper', 'expersq'],y = 'lwage', endogenous=['educ'], instruments = ['motheduc', 'fatheduc','huseduc'] )
 
 
 
-
-class diagnostic(ols_lr):
-    def __init__(self, class_ols_sr, var_to_test):
-        self.data = class_ols_sr.data
-        self.x  = class_ols_sr.x
-        self.y = class_ols_sr.y
-        self.var_to_test = var_to_test
-        self.method = class_ols_sr.method
-
-    def F_test(self):
-        if self.method == 'non_robust':
-            restricted_var = [i for i in self.x if i not in self.var_to_test]
-            restricted = ols_lr(data=self.data, x=restricted_var, y=self.y)
-            residuals1 = restricted.residuals()
-            ssr1 = np.matmul(np.transpose(residuals1), residuals1)
-            y1_mean = np.mean(restricted.prepare_data().get('Y'))
-            unrestricted = ols_lr(data=self.data, x=self.x, y=self.y)
-            residuals2 = unrestricted.residuals()
-            y2_mean = np.mean(unrestricted.prepare_data().get('Y'))
-            ssr2 = np.matmul(np.transpose(residuals2), residuals2)
-            tss2 = np.sum((unrestricted.prepare_data().get('Y') - y2_mean) ** 2)
-            tss1 = np.sum((restricted.prepare_data().get('Y') - y1_mean) ** 2)
-            r1 = 1 - (ssr1 / tss1)
-            r2 = 1 - (ssr2 / tss2)
-            n = len(unrestricted.prepare_data().get('Y'))
-            k2 = unrestricted.prepare_data().get('X').shape[1]
-            k1 = restricted.prepare_data().get('X').shape[1] - 1
-            one = (r2 - r1) / (1 - r2)
-            two = (n - k2) / k1
-            return {'F': one * two,
-                    'p_value': 1 - scipy.stats.f.cdf(one * two, k1, n - k2)}  # find p-value of F test statistic  }
-
-
-        elif self.method == 'robust':
-            return 'F test is not valid for robust regression. Please use another test'
-
-
-
-
-    def wald_test(self,test = 'testing_zero'):
-
-        if self.method == 'robust':
-            reg = ols_lr(data=self.data, x=self.x, y=self.y, method='robust')
-            # add cons to the key variables
-            labels = self.x + ['cons']
-            # retrive the array of beta coefficieitns
-            B = reg.betas()[0]
-            # transpose for using matmul
-            B_trasp = B.reshape((len(labels), 1))
-
-            # compute the robust variance covariance matrix of B
-            xxinv = np.linalg.inv(np.matmul(np.transpose(reg.prepare_data().get('X')), reg.prepare_data().get('X')))
-            B = np.dot(np.transpose(reg.prepare_data().get('X')), reg.prepare_data().get('X') * reg.residuals() ** 2)
-            avar = np.matmul(np.matmul(xxinv, B), xxinv)
-
-            # this is in case we want simply to test bjs = 0
-
-            if test == 'testing_zero':
-                R = np.zeros((len(self.var_to_test), len(self.x) + 1))
-                for q in range(len(self.var_to_test)):
-                    index = labels.index(self.var_to_test[q])
-                    R[q, index] = 1
-                RB = np.matmul(R, B_trasp)
-                r = np.zeros((len(self.var_to_test), 1))
-                RVR = np.linalg.inv(np.matmul(np.matmul(R, avar), np.transpose(R)))
-                RBminus = (RB - r)
-
-                # the wald test is divided by Q to have a F distribution with Q and n-Q degree of freedom
-                W = np.matmul(np.matmul(np.transpose(RBminus), RVR), RBminus) / len(self.var_to_test)
-
-            return {'Wald test': W, 'p_value': 1 - scipy.stats.f.cdf(W, len(self.var_to_test),
-                                                                     len(reg.prepare_data().get('df')) - len(
-                                                                         self.var_to_test))}
-
-
+print(obj.summary())
 
 
